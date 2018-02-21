f5abfe4798136b0e9e068b2dbd91443eb6bba457
David Robinson <admiral.david@gmail.com>
2018-02-21 09:22:36 -0300

    Removing private data

--- api_infos.R
+++
@@ -1,7 +0,0 @@
-library(twitteR)
-api_key = "HetM9U47bmgYsTuEEuUMtuCsw"
-api_secret = "OlL6CfGJDdzVPI1UTWOWmwQvbOWEMsQh36O2OxXZ6B7CNacTlq"
-access_token = "317752515-O3SXSyssftJf2ah62aSSvCsTUx27PwQ2RwIPTUhm"
-access_token_secret = "m37EHWmYInjmOJHcVXPQg3d2Z4HhweXyvLMu2UPWlGKqB"
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
-
